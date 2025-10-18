3bbf2a13908a36e3eecd387e49a66ff6c3473ee4
cgx <2227259065@qq.com>
2025-10-18 11:39:54 +0800

    项目初始化2

---
+++ tlias-web-management/src/main/java/com/itheima/service/impl/DeptServiceImpl.java
@@ -0,0 +1,8 @@
+package com.itheima.service.impl;
+
+import com.itheima.service.DepService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DeptServiceImpl implements DepService {
+}
